e7954ebf66e9bf62e90ed1ba5d27706cac093b9a
David Dang <dtudang@gmail.com>
2014-11-24 16:24:11 -0800

    cleaned up welcome and pages controllers

--- app/controllers/Welcome_Controller.rb
+++ app/controllers/Welcome_Controller.rb
@@ -1,13 +1,5 @@
 class WelcomeController < ApplicationController
   def home
-
-    @quotes = [
-      ['“Git to tha choppa!”','- Arnold Schwarzeneggar'],
-      ['“Why\'s this turtleneck so itchy?”','- Steve Jobs'],
-      ['“I actually loathe pizza.”','- Papa John\'s']
-      ]
-
-
   end
 
 
