135aee4870010a418c3095ed0a17ac8d86fe6d5f
David Dang <dtudang@gmail.com>
2014-12-09 15:49:13 -0800

    created project index, show, edit, projects on index link to show page, updating projects link to show, deleting links to index

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,53 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def show
+  end
+
+  def create
+    @project = Project.new(project_params)
+      if @project.save
+        redirect_to projects_url, notice: 'Project was successfully created'
+      else
+        render :new
+    end
+  end
+
+  def update
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to @project, notice: 'Project was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully deleted.' }
+    end
+  end
+
+
+private
+def set_project
+  @project = Project.find(params[:id])
+end
+
+
+  def project_params
+    params.require(:project).permit(:name)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :tasks, :users
+  resources :tasks, :users, :projects
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -18,6 +18,8 @@ Rails.application.routes.draw do
   get 'tasks' => 'tasks#index'
 
   get 'users' => 'users#index'
+
+  get 'projects' => 'projects#index'
 
 end
 
