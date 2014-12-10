b5a0b61382e775f05d1e9c90edafdda8352fc44f
David Dang <dtudang@gmail.com>
2014-12-09 17:44:54 -0800

    validated that new projects cannot have due dates in past but updates can still be performed on past projects

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,15 @@
 class Task < ActiveRecord::Base
   validates :description, presence: true
+  validates :due_date_cannot_be_in_the_past, :on => :create, confirmation: true
+
+
+
+  def due_date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "cannot be in the past")
+    end
+  end
+
+
 
 end
